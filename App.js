3ae265e3f5796356862d14527170c95c7488311d
HAIURA Andreea-Isabela <79141555+isabellla128@users.noreply.github.com>
2022-05-15 11:24:42 +0300

    update for server
    update for server

--- App.js
+++ App.js
@@ -1,51 +1,8 @@
-/*const http = require('http');
-const fs = require('fs');
-
-const server = http.createServer(function(req, res) {
-
-    console.log(req.url, req.method);
-    res.setHeader('Content-Type', 'text/html');
-
-    let path = './';
-    console.log("req.url=",req.url)
-    switch(req.url) {
-        case '/':
-            path += 'Proiect.html';
-            break;
-        default: path=".".concat(req.url);
-    }
-    
-    console.log("path=",path)
-    fs.readFile(path, (err, data) => {
-        if(err) {
-            console.log(err);
-            res.end();
-        } else {
-            res.write(data);
-            res.end();
-        }
-    })
-    fs.readFile(path, (err, data) => {
-        if(err) {
-            console.log(err);
-            res.end();
-        } else {
-            res.write(data);
-            res.end();
-        }
-    })
-});
-
-server.listen(5500, '127.0.0.1');*/
-
-
 //Create a server that can send back static files
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
 
-//npm i mime-types
-//const lookup = require("mime-types").lookup;
 
 const server = http.createServer((req, res) => {
   //handle the request and send back a static file
@@ -56,7 +13,6 @@ const server = http.createServer((req, res) => {
   /**
    *  /
    *  /index.html
-   *
    *  /main.css
    *  /main.js
    */
@@ -76,26 +32,10 @@ const server = http.createServer((req, res) => {
       //specify the content type in the response
       console.log(`Returning ${path}`);
       res.setHeader("X-Content-Type-Options", "nosniff");
-      //let mime = lookup(path);
-      //res.writeHead(200, { "Content-type": mime });
-        switch (path) {
-        case "Beginner.html":
+        switch (path.slice(-3)) {
+        case "tml":
            res.writeHead(200, { "Content-type": "text/html" });  break;
-        case "Intermediate.html":
-            res.writeHead(200, { "Content-type": "text/html" });  break;
-        case "Advanced.html":
-            res.writeHead(200, { "Content-type": "text/html" });  break;
-        case "Proiect.html":
-           res.writeHead(200, { "Content-type": "text/html" });  break;
-        case "QA.html":
-            res.writeHead(200, { "Content-type": "text/html" });  break;
-        case "Proiect.css":
-            res.writeHead(200, { "Content-type": "text/css" });  break;
-        case "Advanced.css":
-            res.writeHead(200, { "Content-type": "text/css" });  break;
-        case "BegIntAdv.css":
-            res.writeHead(200, { "Content-type": "text/css" });  break;
-        case "QA.css":
+        case "css":
             res.writeHead(200, { "Content-type": "text/css" });  break;
        }
       res.end(content);
